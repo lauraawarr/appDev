6fec358ab8969201c3f3dea26c96be21ec8f8138
Laura Warr <lauraawarr@gmail.com>
2017-04-12 16:51:23 -0400

    Removed user array from results output

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,6 +10,15 @@ use App\Http\Controllers\Controller;
 class UserController extends Controller
 {
 
+	public function getAdmin()
+    {
+        $questions = DB::table('questions')->get();
+
+        $products = DB::table('inventory')->orderBy('id', 'desc')->get();
+
+    	return view('admin', ['questions' => $questions, 'products' => $products]);
+    }
+
     public function getQuestions()
     {
         $questions = DB::table('questions')
@@ -17,37 +26,6 @@ class UserController extends Controller
         				->get();
 
     	return view('questions', ['questions' => $questions]);
-    }
-
-    public function getAjaxResult(Request $request)
-    {
-    	$userArray = $request->userArray;
-    	$topProdScores = [0,0,0,0];
-    	$topProds = [null, null, null, null];
-    	$lastIndex = count( $topProds ) - 1;
-
-        $result = DB::table('inventory')->orderBy('id')->chunk(100, function($products) use ( &$temp, $userArray, &$topProdScores, &$topProds, $lastIndex ){
-
-        	foreach ($products as $product) {
-        		$prodArray = array_map('intval', explode( ',', $product->rankings ));
-        		$prodScore = calcScore( $userArray, $prodArray);
-
-        		$largestIndex = null;
-        		for ($i = $lastIndex; $i > -1; $i--){
-        			if ( $prodScore > $topProdScores[ $i ]){
-        				$largestIndex = $i;
-        			} else {
-        				break;
-        			};
-        		};
-        		if (!is_null($largestIndex)){
-        			$topProds = insertAt( $topProds, $largestIndex, $product );
-        			$topProdScores = insertAt( $topProdScores, $largestIndex, $prodScore );
-    			};
-        	};
-        });
-  
-    	return response()->json(['result' => $topProds, 'userArray' => $userArray, 'scores' => $topProdScores ]);
     }
 
     public function getResult($userArray)
@@ -80,6 +58,20 @@ class UserController extends Controller
         	};
         });
   
-    	return view('results', ['result' => $topProds, 'userArray'=> $userArray]);
+    	return view('results', ['result' => $topProds ]);
+    }
+
+    public function submitProduct(Request $request)
+    {
+    	$product = $request->input;
+  
+    	return response()->json(['result'=> $product ]);
+    }
+
+     public function submitQuestion(Request $request)
+    {
+    	$question = $request->input;
+  
+    	return response()->json(['result'=> $question ]);
     }
 }
