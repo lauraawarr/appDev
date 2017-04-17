cf296989fa6dfaeeee7e980ca0f653be08f9dfe5
Nicole Yu <thenicoleyu@gmail.com>
2017-04-17 15:08:14 -0400

    Fixed JS

--- public/js/questions-overview.js
+++ public/js/questions-overview.js
@@ -1,11 +1,20 @@
 $(window).on("load", function() {
 
-   // object to keep track of user's answers
+   // IMPORTANT
+   // THIS ANSWERS OBJECT SHOULD BE RETRIEVED FROM WHEREVER IT WAS STORED AND USED HERE, INSTEAD OF THE BLANK VERSION BELOW
    var answers = {"q1": "", "q2": "", "q3": "", "q4": ""};
 
    function openAnswers(questionNum) {
       $("#question-" + questionNum + " h6").on("click", function() {
          $("#question-" + questionNum + " input, #question-" + questionNum + " label").removeClass("dn");
+      });
+
+      $(".q" + questionNum).on("click", function() {
+
+         var q = $("input[name=q" + questionNum + "]:checked").val();
+         answers["q" + questionNum] = q;
+         console.log(q);
+         console.log(answers);
       });
    };
 
