a5a188aaf1734955451b2c56759f33912197611d
Chibuzo Uguru <uguru@sheridanc.on.ca>
2017-04-17 14:28:40 -0400

    updated localstorage functionality

--- public/js/comparison.js
+++ public/js/comparison.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
   var compare_items = JSON.parse(localStorage.getItem('array'));
   $.each(compare_items, function() {
-    $('#results').append('<div class="ind-result dtc-ns tc pv4"><h3 class="silver f4 b">'+this.name+'</h3>'+
-      '<div><img src="http://chibuzouguru.com/img/prod3.png" class="bg-light-gray w6"></div>');
+    $('#results').append('<div class="ind-result dtc-ns tc pv4 relative">'+
+      '<div class="pointer absolute top-0-m right-0-m"><img src="fonts/close.svg"></div>'+
+      '<h3 class="silver f4 b">'+this.name+'</h3><div><img src="http://chibuzouguru.com/img/prod3.png" class="bg-light-gray w6">'+
+      '</div></div>');
   });
   // console.log(compare_items);
 });
 
 // need to add a thicker font-weight for avenir
+//<div class="ind-result dtc-ns tc pv4"><h3 class="silver f4 b">'+this.name+'</h3>'+
+  //'<div><img src="http://chibuzouguru.com/img/prod3.png" class="bg-light-gray w6"></div>
--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -12,8 +12,8 @@
     <body class="avenir-next pa4 bg-washed-blue fw3 f5">
       <!-- Used Nicole's navigation and styling -->
         <nav class="pb4 bb bw1 b--lightest-blue flex justify-start">
-          <a href="" class="link dark-gray mr4 pb2 bb bw1 b--blue">Quizzes</a>
-          <a href="" class="link dark-gray mr4 pb2">Create quiz</a>
+          <a href="#" class="link dark-gray mr4 pb2 bb bw1 b--blue">Quizzes</a>
+          <a href="#" class="link dark-gray mr4 pb2">Create quiz</a>
         </nav>
         <div class="results bg-white pv5 mt5">
           <h2 class="tc dark-gray f2 mt0">Results</h2>
@@ -31,7 +31,7 @@
                 <!-- <p>{{ $result[1] -> description}}</p> -->
                 <div><img src="http://chibuzouguru.com/img/prod3.png" class="bg-light-gray w6 mb3"></div>
                 <p class="mt0 f6 blue b view-description-1 pointer"><img src="../fonts/down.svg" class="pr2">DESCRIPTION</p>
-                <p class="dn dn-0">{{$result[1] -> description}}</p>
+                <p class="dn dn-0 ph5 ba">{{$result[1] -> description}}</p>
                 <!-- <p>{{ $result[1] -> img}}</p> -->
                 <button class="pointer bg-blue bn white ph5 pv3 mb2 br2" id="btn2">Save</button>
               </div>
@@ -59,15 +59,19 @@
           <span>
             <a href="../quiz.html" class="blue no-underline mr3 f4 b">Change Answers</a>
             <a href="../start-quiz.html" class="blue no-underline ml3 mr3 f4 b"><img src="../fonts/retake.svg" class="w1 pr1 mb0">Retake Quiz</a>
-            <a href="../comparison.html" class="blue no-underline mr3 f4 b">Compare Results</a>
+            <a href="../comparison.html" class="blue no-underline mr3 ml3 f4 b">Compare Results</a>
           </span>
         </div>
         <!--  script files -->
         <script type="text/javascript">
            var result = <?php echo json_encode( $result ) ?>;
            // create an array in the page and store it in localStorage
-           var stored_array = [];
-           localStorage.setItem('array', JSON.stringify(stored_array));
+           if(localStorage.getItem('array') === null) {
+             var stored_array = [];
+             localStorage.setItem('array', JSON.stringify(stored_array));
+           }
+          //  var stored_array = [];
+          //  localStorage.setItem('array', JSON.stringify(stored_array));
         </script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="../js/results.js"></script>
