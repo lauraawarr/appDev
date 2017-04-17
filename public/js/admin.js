a5229e7d17b7262fa98b0f9be6d76fb1195df075
Laura Warr <lauraawarr@gmail.com>
2017-04-17 14:15:58 -0400

    Updated admin php pages

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,7 +1,8 @@
-/* Listen to click event on any submit button and handles case based on id */
 var imgName = null;
-$('input[type=submit]').on('mousedown', function(ev){
+var imgSrc = null;
 
+/* Listen to click event on any submit button and handles case based on id */
+$('input[type=submit], .submit').on('mousedown', function(ev){
 	var hash = ev.target.id;
 	var trait = ($('#' + hash + '-Trait').val() || null );
 	var name = ($('#' + hash + '-Name').val() || null );
@@ -14,6 +15,9 @@ $('input[type=submit]').on('mousedown', function(ev){
 		image: image
 	};
 	sendData( hash , data);
+
+	if ( name && description && imgSrc ) previewProduct( name, description, imgSrc );
+	if ( trait ) previewTrait( trait );
 
 });
 
@@ -40,13 +44,13 @@ function rankingNav( dir ){
 
 /* Upload an image */
 function previewFile( file ) {
-  var preview = document.querySelector('img');
   var file    = document.querySelector('input[type=file]').files[0];
   var reader  = new FileReader();
 
   reader.addEventListener("load", function () {
-    preview.src = reader.result;
     imgName = file.name;
+    imgSrc = reader.result;
+    $('#image-name').text( imgName );
     console.log('Name: ', imgName);
     // console.log(reader.result);
   }, false);
@@ -56,6 +60,19 @@ function previewFile( file ) {
   }
 };
 
+/* Preview added products as thumbnails */
+function previewProduct( name, des, imgSrc ){
+	$('#products').prepend( '<img src="' + imgSrc + '" height="200">');
+	$('#submitProduct-Name').val(null);
+	$('#submitProduct-Description').val(null);
+	$('#image-name').text( null );
+};
+
+/* Preview added trait in list */
+function previewTrait( trait ){
+	$('#traits').prepend( '<div>' + trait + '</div>');
+	$('#submitTrait-Trait').val(null);
+};
 
 /* Send user's data to the server and returns response */
 function sendData( url, data ){
@@ -78,7 +95,7 @@ function sendData( url, data ){
 		},
 		error: function( data ){
 			console.log( "Error");
-			$('.error').html( data.responseText );
+			$('body').html( data.responseText );
 			console.log( data );
 		},
 		complete: function( data ){
