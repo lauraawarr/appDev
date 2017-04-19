f5cba0b18e5431c6864144033576fdbe07626ef1
Nicole Yu <thenicoleyu@gmail.com>
2017-04-19 12:19:08 -0400

    Added fixed image height on products page. Adding error states

---
+++ public/js/step1.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+   $(".cta-button").on("click", function() {
+      if ("input").val() == "") {
+         $(".error-message").innerHTML = "You still "
+      }
+   })
+})
